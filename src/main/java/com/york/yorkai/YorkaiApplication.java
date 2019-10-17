b8d285ee163b1498f125e99926350cc4f3f6f434
yorksiy <yorksity@outlook.com>
2019-10-18 01:09:11 +0800

    提交文件

---
+++ src/main/java/com/york/yorkai/YorkaiApplication.java
@@ -0,0 +1,13 @@
+package com.york.yorkai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YorkaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YorkaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/york/yorkai/YorkaiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.york.yorkai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YorkaiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
